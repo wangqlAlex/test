dd939a082c0cf8a00ff32f5c62cab3880ee542a8
wangqlAlex <18345067159@163.com>
2016-12-09 10:41:30 +0800

    first version

--- Weather_Client/src/cn/com/webxml/main/Testmain.java
+++ Weather_Client/src/cn/com/webxml/main/Testmain.java
@@ -1,5 +1,11 @@
 package cn.com.webxml.main;
 
+import java.text.DecimalFormat;
+
 public class Testmain {
+	public static void main(String[] args) {
+		DecimalFormat df1 = new DecimalFormat("####.00");
+		System.out.println(df1.format(100000.11));
+	}
 
 }
